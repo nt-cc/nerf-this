cca965458123b4da48f576192e4f98e8fb2260ca
Chris Toph <chris@toph.cc>
2022-11-28 01:00:16 -0400

    Implemented bg responsive home page picks dominant color from bg and colors the UI accordingly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,39 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { FastAverageColor } from 'fast-average-color';
+	import Color from 'color';
+	import { onMount } from 'svelte';
+
+	// TODO: we will fethc this randomly from API for now hardcoded
+	const bg =
+		'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/uncentered/17/17014.jpg';
+
+	// FIXME: this can probably use some refactoring
+	onMount(async () => {
+		const fac: FastAverageColor = new FastAverageColor();
+		const container = document.documentElement;
+		fac
+			.getColorAsync(bg, { algorithm: 'dominant' })
+			.then((result) => {
+				var color: Color = new Color(result.rgb);
+
+				if (result.isDark) {
+					container.style.setProperty('--bg-dom-color', color.fade(0.2).whiten(0.3).rgb().string());
+					container.style.setProperty('--comp', color.fade(0.8).lightness(80).rgb().string());
+					container.style.setProperty('--text-color', '#fff');
+				} else {
+					container.style.setProperty('--bg-dom-color', color.fade(0.2).blacken(0.3).rgb().string());
+					container.style.setProperty('--comp', color.fade(0.8).lightness(20).rgb().string());
+					container.style.setProperty('--text-color', '#000');
+				}
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	});
 </script>
 
-<div id="bg-image" />
+<div id="bg-image" style="--bg-image: url({bg})" />
 
 <div id="nf-container">
 	<slot />
@@ -11,14 +42,13 @@
 <style>
 	#bg-image {
 		@apply h-screen w-screen bg-cover bg-center bg-no-repeat;
-		background-image: url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_47.jpg);
+		background-image: var(--bg-image);
 	}
 
 	#bg-image::after {
 		@apply pointer-events-none absolute h-full w-full bg-opacity-80;
 		content: '';
-		/* TODO: get dominat color from random image for this */
-		background: rgb(106, 125, 165, 0.8);
+		background: var(--bg-dom-color);
 		box-shadow: inset 0 0 10rem black;
 	}
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,8 @@
 	import { faSearch } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<main class="flex h-screen items-center justify-center">
+<main class="flex h-screen flex-col items-center justify-center">
+	<p id="nerf-this">nerf this.</p>
 	<nav id="summoner-srch">
 		<input id="summoner-inpt" type="text" placeholder="Search for Summoner..." />
 		<button id="summoner-sbmt" type="submit">
@@ -13,9 +14,15 @@
 </main>
 
 <style>
+	#nerf-this {
+		@apply p-5 text-7xl font-black;
+		color: var(--text-color);
+	}
+
 	#summoner-srch {
-		@apply flex w-full max-w-lg gap-5 rounded-xl p-5 text-xl font-medium text-white backdrop-blur;
-		background: rgb(165, 146, 106, 0.6);
+		@apply flex w-full max-w-lg gap-5 rounded-xl p-5 text-xl font-medium backdrop-blur;
+		color: var(--text-color);
+		background: var(--comp);
 	}
 
 	#summoner-inpt {

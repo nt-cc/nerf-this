0e82996ce57f0e663399b458dbe0c5b7edb1c194
Chris Toph <chris@toph.cc>
2022-11-27 22:21:51 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../app.css';
+</script>
+
+<div id="bg-image" />
+
+<div id="nf-container">
+	<slot />
+</div>
+
+<style>
+	#bg-image {
+		@apply h-screen w-screen bg-cover bg-center bg-no-repeat;
+		background-image: url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_47.jpg);
+	}
+
+	#bg-image::after {
+		@apply pointer-events-none absolute h-full w-full bg-opacity-80;
+		content: '';
+		/* TODO: get dominat color from random image for this */
+		background: rgb(106, 125, 165, 0.8);
+		box-shadow: inset 0 0 10rem black;
+	}
+
+	#nf-container {
+		@apply absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-20;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faSearch } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<main class="flex h-screen items-center justify-center">
+	<nav id="summoner-srch">
+		<input id="summoner-inpt" type="text" placeholder="Search for Summoner..." />
+		<button id="summoner-sbmt" type="submit">
+			<Fa icon={faSearch} />
+		</button>
+	</nav>
+</main>
+
+<style>
+	#summoner-srch {
+		@apply flex w-full max-w-lg gap-5 rounded-xl p-5 text-xl font-medium text-white backdrop-blur;
+		background: rgb(165, 146, 106, 0.6);
+	}
+
+	#summoner-inpt {
+		@apply w-full bg-transparent placeholder:text-inherit placeholder:text-opacity-80 focus-within:outline-none;
+	}
+
+	#summoner-sbmt {
+		/* TODO MAYBE */
+	}
+</style>
